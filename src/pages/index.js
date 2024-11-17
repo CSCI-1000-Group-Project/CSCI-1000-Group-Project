50f1cd1613af478f92c3fe3bed7f59f2bccccc25
Max Jambor <68831377+MaxJambor@users.noreply.github.com>
2024-11-17 16:01:25 -0700

    Added my own page

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,6 +38,11 @@ export default function Home() {
             <p>Click here to navigate to an example page</p>
           </a>
 
+          <a href='/Max Jambor ' className='card'>
+            <h3>Max's About Me Page &rarr;</h3>
+            <p>Click here to navigate to an example page</p>
+          </a>
+
           <a
             href='https://github.com/vedant29/Project-CSCI-1000'
             className='card'>
